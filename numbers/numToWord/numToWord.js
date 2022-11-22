09283e838041a33170f5f318cfba7549fd37a968
suhuq007@gmail.com <suhuq007@gmail.com>
2022-11-22 19:18:22 +0530

    changes ones&tenths arrays to obj and edited param, fixed a typo mistake

--- numbers/numToWord/numToWord.js
+++ numbers/numToWord/numToWord.js
@@ -1,24 +1,22 @@
 /**
- * takes number value and returns a string converted into words 
- * @param {number} num - takes a number
+ * takes number || string value and returns a string converted into words 
  */
 
-export const convert = function(num) {
-  num = num.toString();
-
-  return getConverted(num);
+const convert = function(value) {
+  value = value.toString();
+  return getConvertedValue(value);
 }
 
-const getConverted = function(num) {
-  const strLength = num.length;
+const getConvertedValue = function(value) {
+  const strLength = value.length;
   //for lakh
   if (strLength <= 7) {
-    const numArr = ('0000000' + num).slice(-7).match(/^(\d{2})(\d{2})(\d{3})$/);
-    return lakh(numArr);
+    const numArr = ('0000000' + value).slice(-7).match(/^(\d{2})(\d{2})(\d{3})$/);
+    return upToLakh(numArr);
   }
 }
 
-const lakh = function(numArr) {
+const upToLakh = function(numArr) {
   let sentence;
   sentence = string(numArr, 1, 0, 1, 'lakh');
   sentence += string(numArr, 2, 0, 1, 'thousand');
@@ -29,30 +27,60 @@ const lakh = function(numArr) {
 }
 
 const string = function(numArr, indexOne, indexTwo, indexThird, value = '') {
-  const ones = onesArr();
-  const tenth = tenthArr();
-  console.log(numArr);
-
+  const onesObj = ones();
+  const tenthObj = tenth();
+  
   if (Number(numArr[indexOne])) {
     //check if the first value of the string is 0 or not if not get the both tenth and ones from respective arrays
 
-    if (value !== 'hundred') return (ones[Number(numArr[indexOne])] || `${tenth[numArr[indexOne][indexTwo]]} ${ones[numArr[indexOne][indexThird]]}`) + ` ${value} `;
+    if (value !== 'hundred') return (onesObj[Number(numArr[indexOne])] || `${tenthObj[numArr[indexOne][indexTwo]]} ${onesObj[numArr[indexOne][indexThird]]}`) + ` ${value} `;
 
     if (value === 'hundred') {
-      const isZero = ones[numArr[indexOne][indexTwo]];
+      const isZero = onesObj[numArr[indexOne][indexTwo]];
 
-      //[023] first digit place on hundred(0) then remove value variable from sentence
-      if (!isZero) return ones[numArr[indexOne][indexTwo]];
+      //[023] if first digit is placed on hundred(0) then remove value variable from sentence
+      if (!isZero) return onesObj[numArr[indexOne][indexTwo]];
 
-      if (isZero) return (ones[Number(numArr[indexOne])] || `${ones[numArr[indexOne][indexTwo]]}`) + ` ${value} `;
+      if (isZero) return (onesObj[Number(numArr[indexOne])] || `${onesObj[numArr[indexOne][indexTwo]]}`) + ` ${value} `;
     }
   } else return '';
 }
 
-function onesArr() {
-  return ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+function ones() {
+  return {
+    0: '',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteenn',
+
+  }
 }
 
-function tenthArr() {
-  return ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+function tenth() {
+  return {
+    2: 'twenty',
+    3: 'thirty',
+    4: 'forty',
+    5: 'fifty',
+    6: 'sixty',
+    7: 'seventy',
+    8: 'eighty',
+    9: 'ninety',
+  };
 }
